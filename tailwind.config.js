145c62d2883a9baad514516bb3bc6c17673cd2b1
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-12-31 10:25:36 +0900

    color pastel added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      bluish: "#90e5da",
+      navish: "#18355b",
+      creamy: "#d4d3c5",
+      oranges: "#ed673c",
+    },
     extend: {},
   },
   plugins: [],
